f9407c045ea810334262079961edbbb6523b6772
Adrian Dolinay <52612686+ad17171717@users.noreply.github.com>
2022-06-28 22:22:19 -0400

    Add files via upload

---
+++ PowerShell/txt_to_csv.ps1
@@ -0,0 +1,7 @@
+﻿#save the text files in a variable within the directory where the ps script is saved
+$payroll_data = get-childitem -file *.txt 
+
+#loop over each of the text files and export them to csv keeping the original file name
+foreach ($file in $payroll_data) {
+    import-csv $file | export-csv "$($file.basename).csv" -notypeinformation
+}
