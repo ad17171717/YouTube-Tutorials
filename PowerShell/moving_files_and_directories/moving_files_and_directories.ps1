45473599915ce3e5f6393abea8154d644ee6fb87
Adrian Dolinay <52612686+ad17171717@users.noreply.github.com>
2022-09-25 16:46:56 -0400

    Add files via upload

---
+++ PowerShell/moving_files_and_directories/moving_files_and_directories.ps1
@@ -0,0 +1,8 @@
+#move single file
+move-item -path C:\Users\Adrian\Downloads\tech\AMZN.csv -destination C:\Users\Adrian\Downloads\stock_data
+
+#move all files in the given directory
+move-item -path C:\Users\Adrian\Downloads\tech\* -destination C:\Users\Adrian\Downloads\stock_data
+
+#move directory
+move-item -path C:\Users\Adrian\Downloads\tech -destination C:\Users\Adrian\Downloads\stock_data
