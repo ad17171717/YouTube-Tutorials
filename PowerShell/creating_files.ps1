845bd2746a3c9b3c210efba9f1218a02229ce547
Adrian Dolinay <52612686+ad17171717@users.noreply.github.com>
2022-10-03 19:59:16 -0400

    Add files via upload

---
+++ PowerShell/creating_files.ps1
@@ -0,0 +1,11 @@
+﻿#create a text file
+New-Item -Path C:\Users\User\Downloads -Name 'test.txt' -ItemType 'file' -Value 'test file'
+
+#create a Python file
+New-Item -Path C:\Users\User\Downloads -Name 'test.py' -ItemType 'file' -Value 'print("test")'
+
+#create multiple text files
+New-Item -ItemType 'file' -Path C:\Users\User\Downloads\test1.txt, C:\Users\User\Downloads\test2.txt
+
+#overwrite a file
+New-Item -Path C:\Users\User\Downloads -Name 'test.txt' -ItemType 'file' -Value 'overwritten file' -force
