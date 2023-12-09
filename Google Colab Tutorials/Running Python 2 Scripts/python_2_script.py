236e43242ab237e5c01cbb0ffdd29f62a2ccdecc
Adrian Dolinay <52612686+ad17171717@users.noreply.github.com>
2023-12-09 14:35:01 +0000

    Add files via upload

---
+++ Google Colab Tutorials/Running Python 2 Scripts/python_2_script.py
@@ -0,0 +1,8 @@
+#Python 2 does not require parentheses for print statements
+print 'Hello World'
+
+#dividing by integers returns an integer data type
+print 7/3
+
+#the xrange function does not exist in Python 3
+print(type(xrange))
