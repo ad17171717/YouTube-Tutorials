0235c5811400e3c42a619e0a12d4af73b11f1eb5
Adrian Dolinay <ad17171717@gmail.com>
2025-06-11 03:58:04 -0600

    Test sec.

--- GitHub Actions/Gitleaks/gitleaks_test.py
+++ GitHub Actions/Gitleaks/gitleaks_test.py
@@ -1,5 +1,5 @@
-# test-push-protection.txt
-AZURE_STORAGE_ACCOUNT_KEY=m8bX5yp6mPgs2epOWoAqzrfgC+GnPpT1OaMd3Z6aZjHCwSkIdDa9Mb8ZAOjsP0qhY6pGrreYsFzF4skBqgpi9Q==
+AZURE_STORAGE_CONNECTION_STRING="DefaultEndpointsProtocol=https;AccountName=myacctstore;AccountKey=m8bX5yp6mPgs2epOWoAqzrfgC+GnPpT1OaMd3Z6aZjHCwSkIdDa9Mb8ZAOjsP0qhY6pGrreYsFzF4skBqgpi9Q==;EndpointSuffix=core.windows.net"
+
 
 if __name__ == "__main__":
     print('Script ran successfully!!')
