c4b48f4afbd5b03eb84a4d749316fac7cbc65333
Adrian Dolinay <ad17171717@gmail.com>
2025-06-11 03:55:41 -0600

    Test sec.

--- GitHub Actions/Gitleaks/gitleaks_test.py
+++ GitHub Actions/Gitleaks/gitleaks_test.py
@@ -1,4 +1,5 @@
-SECRET_KEY = "GlsdfGRpoi4tkljj76j7j"
+# test-push-protection.txt
+AZURE_STORAGE_ACCOUNT_KEY=m8bX5yp6mPgs2epOWoAqzrfgC+GnPpT1OaMd3Z6aZjHCwSkIdDa9Mb8ZAOjsP0qhY6pGrreYsFzF4skBqgpi9Q==
 
 if __name__ == "__main__":
     print('Script ran successfully!!')
